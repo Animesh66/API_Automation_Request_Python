7ecfa90b4cad9f7549b45d3937d22c1b8c169c65
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-11-22 17:04:40 +0530

    Post response modified.

--- post_request/reqres_post_request.py
+++ post_request/reqres_post_request.py
@@ -5,9 +5,10 @@ import jsonpath
 request_uri = "https://reqres.in/api/users"
 file = open('post_request.json', 'r')
 payload = file.read()
-payload_json = json.loads(payload)
+payload_json = json.loads(payload)  # Deserialize the payload file to computer readable format
 post_response = requests.post(request_uri, data=payload_json)
 print(post_response.content)
 print(post_response.status_code)
-fetch_id = jsonpath.jsonpath(post_response.json(), "id")
-print(fetch_id[0])
+fetch_id = jsonpath.jsonpath(post_response.json(), "id")  # Reads the "id" key from the response JSON and
+# returns a list of values of the given key here for the "id" key
+print(fetch_id[0])  # Display the first value of the "id" key
