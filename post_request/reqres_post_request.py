e6d989d0146f354fe0fd6eb8d52cd5136433a895
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-11-23 19:29:27 +0530

    Post response modified.

--- post_request/reqres_post_request.py
+++ post_request/reqres_post_request.py
@@ -1,6 +1,7 @@
 import requests
 import json
-import jsonpath
+from jsonpath import jsonpath
+
 
 request_uri = "https://reqres.in/api/users"
 file = open('post_request.json', 'r')
@@ -8,8 +9,8 @@ payload = file.read()
 payload_json = json.loads(payload)  # Converts the file content to a list of dictionary
 print(payload_json)
 post_response = requests.post(request_uri, data=payload_json)
-print(post_response.content)
+print(post_response.text)
 print(post_response.status_code)
-fetch_id = jsonpath.jsonpath(post_response.json(), "id")  # Reads the "id" key from the response JSON and
+fetch_id = jsonpath(post_response.json(), "id")  # Reads the "id" key from the response JSON and
 # returns a list of values of the given key here for the "id" key
-print(fetch_id)  # Display the first value of the "id" key
+print(fetch_id[0])  # Display the first value of the "id" key
