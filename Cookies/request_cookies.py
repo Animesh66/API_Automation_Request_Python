bc02c893f7233bcae4aad42151772c677ed7ef86
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-11-25 18:08:00 +0530

    Cookies Section Added.

--- Cookies/request_cookies.py
+++ Cookies/request_cookies.py
@@ -1,10 +1,12 @@
 import requests
-import json
-from jsonpath import jsonpath
 
-cookie_name = {"session_id": "cookie"}
 
+# Send Cookies to Server
+
+cookie_name = {"session_id": "cookie_12098712"}
 request_uri = "https://httpbin.org/cookies"
 response_cookies = requests.get(request_uri,cookies=cookie_name)
 print(response_cookies.text)
 print(response_cookies.cookies)
+
+# Receive Cookies from Server
