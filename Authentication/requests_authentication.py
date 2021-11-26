952aa673f105b04866656d9d7880f80d61416e05
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-11-26 18:35:14 +0530

    Authentication script added.

--- Authentication/requests_authentication.py
+++ Authentication/requests_authentication.py
@@ -2,3 +2,14 @@ import requests
 from requests.auth import HTTPBasicAuth
 from requests.auth import HTTPDigestAuth
 from requests_oauthlib import OAuth1
+
+#    ******* Basic Authentication *******
+
+
+#    ******* Bearer Authentication *******
+
+
+#    ******* Digestive Authentication *******
+
+
+#    ******* OAuth1 Authentication *******
