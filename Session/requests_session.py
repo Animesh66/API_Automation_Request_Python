9690497a371aa4293e7b1ee386504995394952eb
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-11-26 18:02:22 +0530

    Session script added.

--- Session/requests_session.py
+++ Session/requests_session.py
@@ -10,3 +10,12 @@ session.get(set_cookie_url,params=company)  # setting the cookie in the session
 response = session.get(get_cookie_url)
 print(response.text)
 response.close()
+
+# Creating a new session
+
+session_two = requests.Session()
+request_header = {"Content-Type": "multipart/form-data"}
+session_two_header = session_two.headers.update(request_header)
+response_two = session_two.get("https://httpbin.org", headers=session_two_header)
+print(response_two.request.headers)
+print(response_two.headers)
