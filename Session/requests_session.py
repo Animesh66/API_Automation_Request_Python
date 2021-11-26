1d8b61581d0171b9ad679b1120d8e8a1b1028ba4
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-11-26 16:58:50 +0530

    Session script added.

--- Session/requests_session.py
+++ Session/requests_session.py
@@ -1,1 +1,11 @@
 import requests
+
+session = requests.Session()  # creating a session
+employee = {"Employee": "Animesh"}
+company = {"Company": "EPAM"}
+set_cookie_url = "https://httpbin.org/cookies/set"
+get_cookie_url = "https://httpbin.org/cookies"
+session.get(set_cookie_url, params=employee)  # setting the cookie in the session
+session.get(set_cookie_url,params=company)  # setting the cookie in the session
+response = session.get(get_cookie_url)
+print(response.text)
