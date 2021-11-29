053ad33d56a6ecf57b8b313e590ada9979d4cbf4
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-11-29 13:52:57 +0530

    Authentication script added.

--- Authentication/requests_authentication.py
+++ Authentication/requests_authentication.py
@@ -5,9 +5,17 @@ from requests_oauthlib import OAuth1
 
 #    ******* Basic Authentication *******
 
+basic_auth_url = "https://httpbin.org/basic-auth/user/passwd"
+basic_response = requests.get(basic_auth_url, auth=HTTPBasicAuth('user', 'passwd1'))
+print("Basic Auth status code: ", basic_response.status_code)
 
 #    ******* Bearer Authentication *******
 
+bearer_Auth_url = "https://httpbin.org/bearer"
+bearer_header = {"Authorization": "Bearer {Access-Token}"}
+bearer_response = requests.get(bearer_Auth_url, headers=bearer_header)  # Bearer token is passed as a header
+print("Bearer Auth status code: ", bearer_response.status_code)
+print("Bearer Response: ", bearer_response.text)
 
 #    ******* Digestive Authentication *******
 
