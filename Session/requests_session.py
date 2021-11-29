637e08c8f6f5aaf48286c36d620e2561464fcdaa
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-11-29 17:30:22 +0530

    Authentication script added.

--- Session/requests_session.py
+++ Session/requests_session.py
@@ -1,19 +1,20 @@
 import requests
+from requests import Session
 
-session = requests.Session()  # creating a session
+session = Session()  # creating a session
 employee = {"Employee": "Animesh"}
 company = {"Company": "EPAM"}
 set_cookie_url = "https://httpbin.org/cookies/set"
 get_cookie_url = "https://httpbin.org/cookies"
 session.get(set_cookie_url, params=employee)  # setting the cookie in the session
-session.get(set_cookie_url,params=company)  # setting the cookie in the session
+session.get(set_cookie_url, params=company)  # setting the cookie in the session
 response = session.get(get_cookie_url)
 print(response.text)
 response.close()
 
 # Creating a new session
 
-session_two = requests.Session()
+session_two = Session()
 request_header = {"Content-Type": "multipart/form-data"}
 session_two_header = session_two.headers.update(request_header)
 response_two = session_two.get("https://httpbin.org", headers=session_two_header)
