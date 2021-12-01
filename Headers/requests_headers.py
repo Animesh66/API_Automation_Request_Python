2aa1037f826aa8bfc263da01a829d46d6d42130c
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-12-01 11:48:21 +0530

    header script added.

--- Headers/requests_headers.py
+++ Headers/requests_headers.py
@@ -1,1 +1,12 @@
 import requests
+
+request_uri = "https://reqres.in/api/users"
+query_params = {'page': '2', 'delay': ['3', '5']}  # This is used for generating the query parameters
+request_header = {'Accept': 'application/json'}
+get_response = requests.get(request_uri, params=query_params,headers=request_header)
+print(get_response.url)
+print(get_response.status_code)
+print(get_response.content)
+print(get_response.headers['Content-Type'])
+print(get_response.headers.get("Content-Type"))
+print(get_response.cookies)
