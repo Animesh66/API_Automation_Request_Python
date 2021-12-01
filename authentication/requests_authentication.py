7dedb051f2ea057a75c6f617d7dd581bd6dac906
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-12-01 14:27:59 +0530

    header script added.

--- authentication/requests_authentication.py
+++ authentication/requests_authentication.py
@@ -3,13 +3,13 @@ from requests.auth import HTTPBasicAuth
 from requests.auth import HTTPDigestAuth
 from requests_oauthlib import OAuth1
 
-#                ******* Basic Authentication *******
+#                ******* Basic authentication *******
 
 basic_auth_url = "https://httpbin.org/basic-auth/user/passwd"
 basic_response = requests.get(basic_auth_url, auth=HTTPBasicAuth('user', 'passwd1'))
 print("Basic Auth status code: ", basic_response.status_code)
 
-#               ******* Bearer Authentication *******
+#               ******* Bearer authentication *******
 
 bearer_Auth_url = "https://httpbin.org/bearer"
 bearer_header = {"Authorization": "Bearer {Access-Token}"}
@@ -17,7 +17,7 @@ bearer_response = requests.get(bearer_Auth_url, headers=bearer_header)  # Bearer
 print("Bearer Auth status code: ", bearer_response.status_code)
 print("Bearer Response: ", bearer_response.text)
 
-#               ******* Digestive Authentication *******
+#               ******* Digestive authentication *******
 
 digest_Auth_url = "https://httpbin.org/digest-auth/auth/user/passwd"
 digest_response = requests.get(digest_Auth_url, auth=HTTPDigestAuth('user', 'passwd'))  # Bearer token is passed as a
@@ -25,7 +25,7 @@ digest_response = requests.get(digest_Auth_url, auth=HTTPDigestAuth('user', 'pas
 print("Digest Auth status code: ", digest_response.status_code)
 print("Digest Response: ", digest_response.text)
 
-#               ******* OAuth1 Authentication *******
+#               ******* OAuth1 authentication *******
 
 oauth_url = "https://authorization-server.com/token"
 oauth = OAuth1('grant_type=authorization_code', 'client_id=hajshdah1212')
--- cookies/request_cookies.py
+++ cookies/request_cookies.py
@@ -1,7 +1,7 @@
 import requests
 from requests.cookies import RequestsCookieJar
 
-# Send Cookies to Server
+# Send cookies to Server
 
 cookie_name = {"session_id": "cookie_12098712"}
 request_uri = "https://httpbin.org/cookies"
@@ -9,7 +9,7 @@ response_cookies = requests.get(request_uri, cookies=cookie_name)
 print(response_cookies.text)
 print(response_cookies.cookies)
 
-# Receive Cookies from Server
+# Receive cookies from Server
 
 response_ggl = requests.get("https://google.com")
 items = response_ggl.cookies.items()  # This will return a list of tuples
